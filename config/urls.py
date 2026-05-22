57166c1772550bd6989a067b97ce01868241102f
adnanfarooqui01 <adnanfarooqui811@gmail.com>
2026-05-22 21:52:37 +0530

    Fix Media

--- config/urls.py
+++ config/urls.py
@@ -39,5 +39,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    # In production, serve media through WhiteNoise (not recommended for large files)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
